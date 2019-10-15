77e9787ffea0ed1581cc931fcffbdfac5f500ebc
roboslyq <robosloveu@163.com>
2019-10-15 18:09:26 +0800

    updated

--- com/roboslyq/usercenter/logon/logon.py
+++ com/roboslyq/usercenter/logon/logon.py
@@ -6,8 +6,7 @@ def logon(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         print(username, password)
-        reshtml = 'welcome.html'
         if username != 'roboslyq' or password != '123456':
-            reshtml = 'error.html'
-
-    return render(request, reshtml, {'username': username, 'password': password})
+            return render(request, 'error.html', {'errorcode': '999999', 'errormessage': '用户名或密码错误'})
+        else:
+            return render(request, 'welcome.html', {'username': username, 'password': password})
--- web_django/settings.py
+++ web_django/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '&+=%n$@#%a7am#2e$6+a=sfc8@%)h7nbxbv6w(wa8i$yc4yf$u'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'web_django.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -114,7 +109,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
@@ -123,3 +117,18 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+# mysql配置添加 roboslyq
+import pymysql  # 一定要添加这两行！
+
+pymysql.install_as_MySQLdb()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'NAME': 'test',  # 数据库名，先前创建的
+        'USER': '1test_user',  # 用户名，可以自己创建用户
+        'PASSWORD': 'q2w3e4r(A',  # 密码
+        'HOST': '147.93.201.88',  # mysql服务所在的主机ip
+        'PORT': '3306',  # mysql服务端口
+    }
+}
